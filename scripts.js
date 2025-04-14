af27a531d8f7a9624bce49a5201e616776d7aa38
Robert Bogan <rajbogan@outlook.com>
2025-04-13 20:38:37 -0700

    Render only the goalkeepers

--- scripts.js
+++ scripts.js
@@ -11,12 +11,29 @@ fetch('player_dataset.json')
   .then(response => response.json())
   .then(data => {
     console.log("JSON Player Data Loaded Successfully!");
-    renderAllPlayers(data); // Render all players
+    //renderAllPlayers(data); // Render all players
+    renderOnlyGoalkeepers(data); // Render only goalkeepers
   })
   .catch(error => {
     console.error("Error loading JSON:", error);
   });
 
+  function renderOnlyGoalkeepers(allPlayers) {
+    const playerDiv = document.getElementById('playerList');
+    playerDiv.innerHTML = '';
+  
+    const allGoalies = allPlayers.filter(player => player.position === 'GK');
+  
+    const goalkeeperHeading = document.createElement('h3');
+    goalkeeperHeading.textContent = 'Goalkeepers';
+    playerDiv.appendChild(goalkeeperHeading);
+  
+    allGoalies.forEach(player => {
+      const entry = buildPlayerEntry(player);
+      playerDiv.appendChild(entry);
+    });
+  }
+  
   // List all players in the sortable list
   function renderAllPlayers(allPlayers) {
     const playerDiv = document.getElementById('playerList');
